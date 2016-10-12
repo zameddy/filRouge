2ca10cd0d718be1bf9059576911345a85c889ea6
zameddy <eddy.zam@gmail.com>
2016-10-12 11:29:31 +0200

    ClientAdresseModel

---
+++ MiniFilRouge/Models/ClientAdresseModel.cs
@@ -0,0 +1,38 @@
+﻿using MiniFilRouge.Metier;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MiniFilRouge.Models
+{
+    public class ClientAdresseModel
+    {
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Nom requis")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email requis")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Login requis")]
+        [Column(TypeName = "varchar")]
+        [StringLength(250)]
+        [Index("Iusername", IsUnique = true)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Mot de passe requis")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage = "Confirmer votre mot de passe")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+
+        public string NumRue { get; set; }
+        public string NomRue { get; set; }
+        public string CodePostal { get; set; }
+        public string Ville { get; set; }
+    }
+}
